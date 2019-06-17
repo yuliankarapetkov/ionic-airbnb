34b7486f1fcd378ce639ab7064765ab3eedc3597
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-06-17 17:43:26 +0300

    added a dummy auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+// tslint:disable-next-line: variable-name
+  private _isAuthenticated = false;
+
+  get isAuthenticated(): boolean {
+    return this._isAuthenticated;
+  }
+
+  constructor() { }
+
+  logIn(): void {
+    this._isAuthenticated = true;
+  }
+
+  logOut(): void {
+    this._isAuthenticated = false;
+  }
+}
